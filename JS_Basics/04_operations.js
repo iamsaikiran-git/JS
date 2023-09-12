ba6528d053de50c0aec665e346157a74f2f2835c
iamsaikiran-git <66454021+iamsaikiran-git@users.noreply.github.com>
2023-09-12 06:49:12 +0000

    Arithematic and other operators in js

---
+++ JS_Basics/04_operations.js
@@ -0,0 +1,19 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//Normal Arithematic operators work similar to othe rlanguages  +,-,*,**,/,%
+console.log(Math.floor(Number(2/3))); // Division is floating point division so you need to convert according to your needs
+
+
+// String concatenation is similar to java and python
+console.log("sai"+ "Kiran");
+
+console.log(1+2)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2"+3);
+console.log(1+2+"3");   //precedence of the initial variable type in the beginning does matterin computation
+
+console.log(1+true) // gives 2 since true internally has 1 integer value
+console.log(2**"") // 2 power 0 is 1 
