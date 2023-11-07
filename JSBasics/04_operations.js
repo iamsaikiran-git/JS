0e1730794ad49bb5c03072aba8419abce0da342a
iamsaikiran-git <66454021+iamsaikiran-git@users.noreply.github.com>
2023-11-07 11:09:44 +0000

    Made changes and added explaination for comparisiona and strict check in javascript

--- JSBasics/04_operations.js
+++ JSBasics/04_operations.js
@@ -17,3 +17,21 @@ console.log(1+2+"3");   //precedence of the initial variable type in the beginni
 
 console.log(1+true) // gives 2 since true internally has 1 integer value
 console.log(2**"") // 2 power 0 is 1 
+
+console.log("----------  Comparisions and related operators -------------------");
+// Comparision of data types and  few miscellaneous cases
+console.log(null > 0); // False
+console.log(null == 0); // False
+console.log(null >= 0); // True 
+// Reason : the comparision and equality checks work differently in javascript that's why they give different result in third case comparision operator actually converts null to 0 while comparing 
+
+console.log(undefined > 0); // False
+console.log(undefined == 0); // False
+console.log(undefined >= 0); // False   // the above case is only miscellaneous to null type and not for undefined type 
+
+
+
+console.log("-------- Strict Check --------------");
+
+console.log("2" === 2); // This checks the data type also along with the value of the things in question
+console.log("2" == 2); // This tries to get the value by converting the data type to possible conversion preference of either types and then compare and give the results
